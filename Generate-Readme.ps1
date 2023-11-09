47b5270dcfbb8aee3c49d6c432e167353ebf9cd4
Adam Koch <me@adamko.ch>
2023-11-09 12:21:47 -0500

    Added .pkgmeta for Curseforge packaging.

---
+++ Generate-Readme.ps1
@@ -0,0 +1,29 @@
+$readmeFile = "{0}\README.md" -f $PSScriptRoot
+$readme = @"
+# AdiBags_EmeraldDream
+Creates an AdiBags filter for items from the Emerald Dream, added in WoW Patch 10.2: Guardians of the Dream.
+
+https://legacy.curseforge.com/wow/addons/adibags-emeralddream
+
+## Emerald Dream Items
+
+{0}
+"@
+
+$found = @()
+foreach ($line in Get-Content -Path ("{0}\AdiBags_EmeraldDream.lua" -f $PSScriptRoot)) {
+	if ($line -match "\[([0-9]{6})\] = true, -- (.+)") {
+		$found += "* [{0}]\(https://wowhead.com/item={1})" -f ($Matches[2], $Matches[1])
+	}
+}
+$found = $found | Sort-Object
+
+
+
+if ($found.Count -gt 0) {
+	if (Test-Path -Path $readmeFile) {
+		Remove-Item -Path $readmeFile -Force
+	}
+	
+	Add-Content -Path $readmeFile -Value ($readme -f (($found -join "`r`n") | Out-String))
+}
